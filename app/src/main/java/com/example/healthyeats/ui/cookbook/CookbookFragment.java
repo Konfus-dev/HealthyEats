d349796fed85dcca86db63eda4cb8e0b8c5ada40
Taichen <jatr812@gmail.com>
2021-02-15 13:20:05 -0800

    Added search bar

--- app/src/main/java/com/example/healthyeats/ui/cookbook/CookbookFragment.java
+++ app/src/main/java/com/example/healthyeats/ui/cookbook/CookbookFragment.java
@@ -23,13 +23,7 @@ public class CookbookFragment extends Fragment {
         cookbookViewModel =
                 new ViewModelProvider(this).get(CookbookViewModel.class);
         View root = inflater.inflate(R.layout.fragment_cookbook, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        cookbookViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
