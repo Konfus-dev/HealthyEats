545ee9d046c75998201c3f06d7f1e09a30482f77
Taichen <jatr812@gmail.com>
2021-02-18 15:52:12 -0800

    Working on fragment

--- app/src/main/java/com/healthyeats/controller/CookbookFragment.java
+++ app/src/main/java/com/healthyeats/controller/CookbookFragment.java
@@ -6,11 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.healthyeats.R;
 import com.healthyeats.view.CookbookViewModel;
+
+import java.text.BreakIterator;
 
 public class CookbookFragment extends Fragment {
 
@@ -21,6 +25,13 @@ public class CookbookFragment extends Fragment {
         cookbookViewModel =
                 new ViewModelProvider(this).get(CookbookViewModel.class);
         View root = inflater.inflate(R.layout.fragment_cookbook, container, false);
+
+        cookbookViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
 
         return root;
     }
