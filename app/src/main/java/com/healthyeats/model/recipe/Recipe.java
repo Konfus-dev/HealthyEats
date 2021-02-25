b750bfdd3a993e46c9de4d2f09925a5b495d3fa8
InfernalJax <roundsj@wwu.edu>
2021-02-25 12:42:01 -0800

    Small tweaks.

--- app/src/main/java/com/healthyeats/model/recipe/Recipe.java
+++ app/src/main/java/com/healthyeats/model/recipe/Recipe.java
@@ -162,6 +162,16 @@ public class Recipe implements Comparable<Recipe> {
     }
 
 
+    //ToDo:
+    // Patch up exporting ingredients to the grocery list, make sure there are not duplicates,
+    // if there are more than a certain amount increment else ignore and keep one
+    // ex: no need to have 2x 1 cup of flower, just have flower
+    public void exportToGroceryList(List<GroceryItem> list) {
+        for (int i = 0; i < ingredients.length; i++) {
+            list.add(ingredients[i].toGroceryItem());
+        }
+    }
+
     @Override
     public int compareTo(Recipe g){
         return this.name.compareTo(g.name);
@@ -182,16 +192,6 @@ public class Recipe implements Comparable<Recipe> {
             }
         }
     };
-
-    //ToDo:
-    // Patch up exporting ingredients to the grocery list, make sure there are not duplicates,
-    // if there are more than a certain amount increment else ignore and keep one
-    // ex: no need to have 2x 1 cup of flower, just have flower
-    public void exportToGroceryList(List<GroceryItem> list) {
-        for (int i = 0; i < ingredients.length; i++) {
-            list.add(ingredients[i].toGroceryItem());
-        }
-    }
 
     //Comparator to sort array by item name alphabetically from Z-A
     public static final Comparator<Recipe> recipeZAComparator = new Comparator<Recipe>() {
