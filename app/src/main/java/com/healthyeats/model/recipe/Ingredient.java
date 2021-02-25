637fd9edd934a0b6b5eac283b52ae845921d41ba
SayHello-Creator <63468146+SayHello-Creator@users.noreply.github.com>
2021-02-25 09:37:03 -0800

    Update Ingredient.java

--- app/src/main/java/com/healthyeats/model/recipe/Ingredient.java
+++ app/src/main/java/com/healthyeats/model/recipe/Ingredient.java
@@ -7,6 +7,7 @@ public class Ingredient {
     private String name;
     private String measurement;
     private String amount;
+    private boolean userAlreadyHasIngredient;
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
@@ -16,6 +17,10 @@ public class Ingredient {
 
     public String getAmount() { return amount; }
     public void setAmount(String amount) { this.amount = amount; }
+
+    public boolean CheckIfUserHasIngredient() {
+        return userAlreadyHasIngredient;
+    }
 
     public GroceryItem toGroceryItem() {
         GroceryItem item = new GroceryItem();
