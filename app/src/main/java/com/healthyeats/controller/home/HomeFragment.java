8b089258e63035a12e1db802b0a112661f3dd0f2
Taichen <jatr812@gmail.com>
2021-02-26 15:29:58 -0800

    Getting home fragments ready

--- app/src/main/java/com/healthyeats/controller/home/HomeFragment.java
+++ app/src/main/java/com/healthyeats/controller/home/HomeFragment.java
@@ -12,6 +12,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.healthyeats.R;
 import com.healthyeats.model.viewModels.HomeViewModel;
+import com.healthyeats.model.viewModels.ViewHelper;
 
 public class HomeFragment extends Fragment {
 
@@ -24,7 +25,17 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        ViewHelper obj = new ViewHelper();
 
         return root;
+    }
+
+    public void populateThisWeeksMeals(View root, ViewHelper obj) {
+        //Get list of This Weeks Meals
+//        thisweeksMeals = root.findViewById(R.id.w)
+    }
+
+    public void populateYourFavorites(View root, ViewHelper obj) {
+        //Get list of Your Favorites
     }
 }
