5087dcfc2f5cc34989acb3459558e8e097388241
Taichen <jatr812@gmail.com>
2021-03-05 12:32:02 -0800

    Got front end to populate grocerylist information

--- app/src/main/java/com/healthyeats/controller/grocery/GrocerylistFragment.java
+++ app/src/main/java/com/healthyeats/controller/grocery/GrocerylistFragment.java
@@ -1,5 +1,6 @@
 package com.healthyeats.controller.grocery;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
@@ -39,34 +40,51 @@ public class GrocerylistFragment extends Fragment {
         return root;
     }
 
-    public ImageButton createImageButton(View root) {
+    public ImageButton createImageButton(View root, ViewHelper rec) {
         ImageButton but = new ImageButton(new ContextThemeWrapper(getActivity(), R.style.groceryItemImage), null, 0);
         but.setImageDrawable(root.getResources().getDrawable(R.drawable.checkgreenbackground_icon));
+        but.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, rec.toDP(90, getContext())));
 
         return but;
     }
 
-    public TextView createGroceryItemName(String itemName) {
+    @SuppressLint("ResourceAsColor")
+    public TextView createGroceryItemName(String itemName, ViewHelper rec) {
         TextView name = new TextView(getContext());
-        name.setTextAppearance(getActivity(), R.style.groceryItemName);
         name.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        name.setText(itemName);
 
+        name.setTextSize(rec.toDP(7, getContext()));
+
+        // Margin
+        ViewGroup.MarginLayoutParams layoutParams =
+                (ViewGroup.MarginLayoutParams) name.getLayoutParams();
+        layoutParams.setMargins(rec.toDP(5, getContext()), rec.toDP(5, getContext()), 0, 0);
+        name.requestLayout();
+
+        name.setText(itemName);
         return name;
     }
 
-    public TextView createGroceryItemQuantity(String quantity) {
+    public TextView createGroceryItemQuantity(String quantity, ViewHelper rec) {
         TextView quan = new TextView(getContext());
         quan.setTextAppearance(getActivity(), R.style.groceryItemPrice);
         quan.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        quan.setText(quantity);
+        quan.setText("Quantity: " + quantity);
+
+        quan.setTextSize(rec.toDP(5, getContext()));
+
+        // Margin
+        ViewGroup.MarginLayoutParams layoutParams =
+                (ViewGroup.MarginLayoutParams) quan.getLayoutParams();
+        layoutParams.setMargins(rec.toDP(5, getContext()), rec.toDP(5, getContext()), 0, 0);
+        quan.requestLayout();
 
         return quan;
     }
 
     public void createInnerLayoutCard(LinearLayout parent, ViewHelper rec, View root, Ingredient ingred) {
         LinearLayout inner = new LinearLayout(getContext());
-        inner.setLayoutParams(new LinearLayout.LayoutParams(rec.toDP(150, getContext()), ViewGroup.LayoutParams.MATCH_PARENT));
+        inner.setLayoutParams(new LinearLayout.LayoutParams(rec.toDP(150, getContext()), rec.toDP(160, getContext())));
 
         // Margin
         ViewGroup.MarginLayoutParams layoutParams =
@@ -74,10 +92,14 @@ public class GrocerylistFragment extends Fragment {
         layoutParams.setMargins(rec.toDP(30, getContext()), rec.toDP(10, getContext()), 0, 0);
         inner.requestLayout();
 
+        inner.setOrientation(LinearLayout.VERTICAL);
+
+        inner.setBackgroundColor(getResources().getColor(R.color.white));
+
         parent.addView(inner);
-        inner.addView(createImageButton(root));
-        inner.addView(createGroceryItemName(ingred.getName()));
-        inner.addView(createGroceryItemQuantity(ingred.getAmount()));
+        inner.addView(createImageButton(root, rec));
+        inner.addView(createGroceryItemName(ingred.getName(), rec));
+        inner.addView(createGroceryItemQuantity(ingred.getAmount(), rec));
     }
 
     public void populateGroceryList(View root) {
