1c4f187c3711f209fbd48918dfde212469edf0c0
Taichen <jatr812@gmail.com>
2021-03-10 08:43:47 -0800

    Adding population of spinners

--- app/src/main/java/com/healthyeats/controller/account/AccountFragment.java
+++ app/src/main/java/com/healthyeats/controller/account/AccountFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -20,9 +22,34 @@ public class AccountFragment extends Fragment {
         accountViewModel =
                 new ViewModelProvider(this).get(AccountViewModel.class);
         View root = inflater.inflate(R.layout.fragment_account, container, false);
-
+        populateSpinner(root);
+        
         return root;
     }
 
+    public void populateSpinner(View root) {
+        Spinner notificationSpinner = (Spinner) root.findViewById(R.id.spinnerNotifications);
+        ArrayAdapter<CharSequence> adapterNotif = ArrayAdapter.createFromResource(getContext(),
+                R.array.notifications_array, android.R.layout.simple_spinner_item);
+        adapterNotif.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        notificationSpinner.setAdapter(adapterNotif);
 
+        Spinner languageSpinner = (Spinner) root.findViewById(R.id.spinnerLanguage);
+        ArrayAdapter<CharSequence> adapterLanguage = ArrayAdapter.createFromResource(getContext(),
+                R.array.language_array, android.R.layout.simple_spinner_item);
+        adapterLanguage.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(adapterLanguage);
+
+        Spinner currencySpinner = (Spinner) root.findViewById(R.id.spinnerCurrency);
+        ArrayAdapter<CharSequence> adapterCurrency = ArrayAdapter.createFromResource(getContext(),
+                R.array.currency_array, android.R.layout.simple_spinner_item);
+        adapterCurrency.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        currencySpinner.setAdapter(adapterCurrency);
+
+        Spinner measurementSpinner = (Spinner) root.findViewById(R.id.spinnerMeasurement);
+        ArrayAdapter<CharSequence> adapterSpinner = ArrayAdapter.createFromResource(getContext(),
+                R.array.measurement_array, android.R.layout.simple_spinner_item);
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        measurementSpinner.setAdapter(adapterSpinner);
+    }
 }
