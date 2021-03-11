0ac140d5d139e4abb6e68f64cc9c4a0c5a2ba1c3
Taichen <jatr812@gmail.com>
2021-03-10 17:55:42 -0800

    Added settings fragment

--- app/src/main/java/com/healthyeats/controller/settings/SettingsFragment.java
+++ app/src/main/java/com/healthyeats/controller/settings/SettingsFragment.java
@@ -1,4 +1,23 @@
 package com.healthyeats.controller.settings;
 
-public class SettingsFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.healthyeats.R;
+
+public class SettingsFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        Bundle args = getArguments();
+
+
+        View root = inflater.inflate(R.layout.fragment_recipe, container, false);
+
+        return root;
+    }
 }
